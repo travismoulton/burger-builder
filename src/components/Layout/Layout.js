ea138ba9925aa0bd449213d2413051924ad6c3be
Travis <travis.moulton002@gmail.com>
2021-02-05 16:12:34 -0500

    added proptypes to BurgerIngredient

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+
+import classes from './Layout.module.css';
+
+const Layout = (props) => (
+  <Fragment>
+    <div>Toolbar, SideDrawer, Backdrop</div>
+    <main className={classes.Content}>{props.children}</main>
+  </Fragment>
+);
+
+export default Layout;
