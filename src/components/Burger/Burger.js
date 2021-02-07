e4550d430b24bf5d23ff4fcf0290257c5ddd0695
Travis <travis.moulton002@gmail.com>
2021-02-06 19:12:16 -0500

    worked on buger builder component and buildcontrols

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import classes from './Burger.module.css';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+
+const Burger = (props) => {
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map((ingredientKey) => {
+      return [...Array(props.ingredients[ingredientKey])].map((_, i) => {
+        return (
+          <BurgerIngredient key={ingredientKey + 1} type={ingredientKey} />
+        );
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+
+  if (!transformedIngredients.length) {
+    transformedIngredients = <p>Please start adding ingredients</p>;
+  }
+  return (
+    <div className={classes.Burger}>
+      <BurgerIngredient type="bread-top" />
+      {transformedIngredients}
+      <BurgerIngredient type="bread-bottom" />
+    </div>
+  );
+};
+
+export default Burger;
